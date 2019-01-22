a213ceb14779e12dce01251122e06d1320ba16af
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-01-22 14:08:32 +0100

    Created the controller functions and view files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  include UsersHelpe
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to user_path(@user)
+  end
+
 end
